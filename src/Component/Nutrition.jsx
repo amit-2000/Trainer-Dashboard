4b0681d1df99351f60c9da3060386fcee33eb923
amit <amitperane8473@gmail.com>
2022-07-24 19:19:48 +0530

    hover done

--- src/Component/Nutrition.jsx
+++ src/Component/Nutrition.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, Component } from "react";
 import ReactTooltip from "react-tooltip";
 import { Text, Box } from "@chakra-ui/react";
 import { PieChart } from "react-minimal-pie-chart";
@@ -8,87 +8,94 @@ import { MinusIcon, AddIcon } from "@chakra-ui/icons";
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import ToolTip from "./Tooltip/Progressbar";
 
-export default class Nutrition extends Component {
-
-  
-
-  render() {
-    return (
-      <Box>
-        <Flex>
-          <Box
-            data-for="main"
-            data-html={true}
-            data-tip="<div style='disply:flex'>
+export const Nutrition = () => {
+  const [hover, sethover] = useState(false);
+  console.log(hover);
+  return (
+    <Box>
+      <Flex>
+        <Box
+          onMouseOver={() => sethover(true)}
+          onMouseOut={() => sethover(false)}
+          data-for="main"
+          data-html={true}
+          data-tip="<div style='disply:flex'>
           <p >Hello world</p> <p></p>Hello world</div>"
-            // bg={"black"}
-            w="32%"
-          >
-            <PieChart
-              data-iscapture="true"
-              lineWidth={22}
-              radius={50}
-              data={[
-                { value: 30, color: "#E38627" },
-                { value: 25, color: "#C13C37" },
-                { value: 20, color: "#6A2135" },
-              ]}
-              // Add font later.
-              label={({ x, y, dx, dy, dataEntry }) => (
-                <svg fill="white">
-                  <text x={x} y={y} dx={dx} dy={dy}>
-                    <tspan x={"1.8rem"} font-size={"19px"} font-weight={600}>
-                      2547
-                    </tspan>
-                    <tspan
-                      x={"1.6rem"}
-                      y={"4.2rem"}
-                      font-size={"15px"}
-                      font-weight={400}
-                    >
-                      calories
-                    </tspan>
-                  </text>
-                </svg>
-              )}
-              labelPosition={0}
-            />
-          </Box>
+          // bg={"black"}
+          w="32%"
+        >
+          <PieChart
+            data-iscapture="true"
+            lineWidth={22}
+            radius={50}
+            data={[
+              { value: 30, color: "#E38627" },
+              { value: 25, color: "#C13C37" },
+              { value: 20, color: "#6A2135" },
+            ]}
+            // Add font later.
+            label={({ x, y, dx, dy, dataEntry }) => (
+              <svg fill="white">
+                <text x={x} y={y} dx={dx} dy={dy}>
+                  <tspan x={"1.8rem"} font-size={"19px"} font-weight={600}>
+                    2547
+                  </tspan>
+                  <tspan
+                    x={"1.6rem"}
+                    y={"4.2rem"}
+                    font-size={"15px"}
+                    font-weight={400}
+                  >
+                    calories
+                  </tspan>
+                </text>
+              </svg>
+            )}
+            labelPosition={0}
+          />
+        </Box>
 
-          <Box>
-            <Flex flexDirection={"row"} ml={3}>
-              <Box>
-                <Box bg={"black"} w={7} borderRadius="6px">
-                  <Center h={5}>
-                    <AddIcon boxSize={2} />
-                  </Center>
-                </Box>
-
-                <Text fontSize={"sm"} fontWeight={700}>
-                  2.5K
-                </Text>
-                <Text fontSize={"xs"} mt="-6px">
-                  target
-                </Text>
-                <Box bg={"black"} w={7} borderRadius="6px">
-                  <Center h={5}>
-                    <MinusIcon boxSize={2} />
-                  </Center>
-                </Box>
+        <Box>
+          <Flex flexDirection={"row"} ml={3}>
+            <Box>
+              <Box bg={"black"} w={7} borderRadius="6px">
+                <Center h={5}>
+                  <AddIcon boxSize={2} />
+                </Center>
               </Box>
 
-              <Center>
-                <Box ml={4} w={8} bg="red" pt={5} pb={5} borderRadius="10px">
-                  <ChevronRightIcon boxSize={6} />
-                </Box>
-              </Center>
-            </Flex>
-          </Box>
-        </Flex>
-        <Box pos={"absolute"} mt={1} ml={-2} style={{ zIndex: "10" }}>
-          <ToolTip />
+              <Text fontSize={"sm"} fontWeight={700}>
+                2.5K
+              </Text>
+              <Text fontSize={"xs"} mt="-6px">
+                target
+              </Text>
+              <Box bg={"black"} w={7} borderRadius="6px">
+                <Center h={5}>
+                  <MinusIcon boxSize={2} />
+                </Center>
+              </Box>
+            </Box>
+
+            <Center>
+              <Box ml={4} w={8} bg="red" pt={5} pb={5} borderRadius="10px">
+                <ChevronRightIcon boxSize={6} />
+              </Box>
+            </Center>
+          </Flex>
         </Box>
+      </Flex>
+      <Box
+        display={hover ? "block" : "none"}
+        pos={"absolute"}
+        mt={1}
+        ml={-2}
+        style={{ zIndex: "10" }}
+      >
+        <ToolTip />
       </Box>
-    );
-  }
-}
+    </Box>
+  );
+};
+
+export default Nutrition;
